8f0ace25eff33021fab62c8918f86f7ac73dd175
DSCRAFT07 <sambazovd@gmail.com>
2025-03-17 17:23:27 +0100

    Custom Handlers

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "OK")
+}
+
+
 func main() {
-	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	// Přidání readiness endpointu
+	mux.HandleFunc("/healthz", healthHandler)
+
+	// Nastavení fileserveru na /app/
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Printf("Server listening on port %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
+
